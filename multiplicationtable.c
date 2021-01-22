3ee1e0e51bf213ea0304f5192c42a6790e332fba
ChiragChakerverty <75806777+ChiragChakerverty@users.noreply.github.com>
2021-01-23 00:28:12 +0530

    Add files via upload

---
+++ multiplicationtable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,a,b,c,d,e,f,g,h,i,j;
+    printf("enter the no. you want multiplication of=\n");
+    scanf("%d" , &num);
+    printf("the table of the chosen no is:-");
+    a=num*1;
+    b=num*2;
+    c=num*3;
+    d=num*4;
+    e=num*5;
+    f=num*6;
+    g=num*7;
+    h=num*8;
+    i=num*9;
+    j=num*10;
+    printf("%d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n" ,a,b,c,d,e,f,g,h,i,j);
+    return 0;
+}
